a56b42979adc9cedf76b84d9739054282879f76b
AlexKarari <karari.alexander@gmail.com>
2019-03-21 14:58:30 +0300

    Class Based Views
    Implementation of class based views alongside function based views.

--- twitter/urls.py
+++ twitter/urls.py
@@ -3,11 +3,20 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+#Function based views
+# urlpatterns = [
+#     url('^$', views.index, name='homepage'),
+#     url('^posted_tweet/', views.view_tweet, name='singleTweet'),
+#     url('^users_tweets/', views.tweet_list, name='usersTweets'),
 
+# ]
+
+
+#Class based views url
 urlpatterns = [
     url('^$', views.index, name='homepage'),
-    url('^posted_tweet/', views.view_tweet, name='singleTweet'),
-    url('^users_tweets/', views.tweet_list, name='usersTweets'),
+    url('^posted_tweet/', views.TweetDetailView, name='singleTweet'),
+    url('^users_tweets/', views.TweetListView, name='usersTweets'),
 
 ]
 
--- twitter/views.py
+++ twitter/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Tweet
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
 
@@ -9,18 +10,35 @@ def index (request):
 
 # C.R.U.D (Create, Retrieve, Update, Delete. List & Search also apply here) tweets
 
+#Funtion based views below
 # Function below RETREIEVES a tweet with a function based view
-def view_tweet(request, tweet_id):
-    tweet = Tweet.objects.get(id=tweet_id) # Get tweet from Database
-    context = {
-        "object": tweet
-    }
-    return render(request, 'all/individual_tweet.html', context)
+# def view_tweet(request, tweet_id):
+#     tweet = Tweet.objects.get(id=tweet_id) # Get tweet from Database
+#     context = {
+#         "object": tweet
+#     }
+#     return render(request, 'all/individual_tweet.html', context)
 
-# Function below displays a list of tweets with a function based view
-def tweet_list(request):
+# # Function below displays a list of tweets with a function based view
+# def tweet_list(request):
+#     queryset = Tweet.objects.all()
+#     context = {
+#         "object_list": queryset
+#     }
+#     return render(request, 'all/tweet_list.html', context)
+
+#Class based views below
+# C.R.U.D (Create, Retrieve, Update, Delete. List & Search also apply here) tweets
+#class based view to retrieve a tweet 
+ 
+class TweetDetailView(DetailView):
+    template_name = "all/individual_tweet.html."
     queryset = Tweet.objects.all()
-    context = {
-        "object_list": queryset
-    }
-    return render(request, 'all/tweet_list.html', context)
+    
+    def get_object(self, tweet_id):
+        return Tweet.objects.get(id=tweet_id)
+
+class TweetListView(ListView):
+    template_name = 'all/tweet_list.html'
+    queryset = Tweet.objects.all()
+
