5f222fcb6b737e686dd63cbba140f2974fb782b6
alexkarari <k.alexander@gmail.com>
2019-06-10 17:59:15 +0300

    Django Crisp forms
    Django crisp forms implemented

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'twitter'
 ]
 
@@ -123,3 +124,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
